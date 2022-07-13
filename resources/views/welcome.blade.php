934e3367fb081d75381f37cae7a43d99949a661a
openopen114 <openopen114@gmail.com>
2022-07-14 00:26:46 +0800

    .env

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   Stanky!
+                  OPENOPEN
                 </div>
 
                 <div class="links">
