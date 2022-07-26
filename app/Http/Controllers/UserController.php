9bf75c56bb0cbd8a807f1c118d6ef092a5fda09f
openopen114 <openopen114@gmail.com>
2022-07-26 16:07:06 +0800

    rrrr

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,24 +17,7 @@ class UserController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {
-
-        syslog(LOG_INFO, "LOG_INFO 1");
-        syslog(LOG_DEBUG, "LOG_DEBUG 2");
-        syslog(LOG_WARNING, "LOG_WARNING 3");
-        syslog(LOG_ERR, "LOG_ERR 4"); 
-
-
-
-        // error_log("error_log 000", 0);
-        // error_log("error_log 111", 1);
-        // error_log("error_log 333", 3);
-
-
-        echo "hello world via echo";
-
-        print "hello world via print";
-
+    { 
 
         $users = DB::select('select * from HRS001MST01 limit 3');
 
